cbdb819faf3711605c7698dc8a9bb9b31d9f0e33
markhorvath <markhorvath7@gmail.com>
2020-03-07 19:35:55 -0500

    useEffect, refactor hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
 
-class App extends React.Component {
-    state = { resource: 'posts' }
-    render() {
+const App = () => {
+    const [resource, setResource] = useState('posts');
         return (
             <div>
                 <div>
-                    <button onClick={() => this.setState({resource: 'posts'})}>Posts</button>
-                    <button onClick={() => this.setState({resource: 'todos'})}>Todos</button>
+                    <button onClick={() => setResource('posts')}>Posts</button>
+                    <button onClick={() => setResource('todos')}>Todos</button>
                 </div>
-                {this.state.resource}
+                <ResourceList resource={resource} />
             </div>
         )
-    }
 }
 
 export default App;
---
+++ src/components/ResourceList.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const ResourceList = ({ resource }) => {
+    const [resources, setResources] = useState([]);
+
+    // const fetchResource = async () => {
+    //     const response = await axios.get(
+    //         `https://jsonplaceholder.typicode.com/${resource}`
+    //     );
+
+    //     setResources(response.data);
+    // };
+
+    //this is a nasty syntax but it's how you use an async await function inside useEffect
+    //essentially just turn it into an immediately invoked function (IIFE)
+    useEffect(() => {
+        // fetchResource(resource)
+        (async resource => {
+            const response = await axios.get(
+                `https://jsonplaceholder.typicode.com/${resource}`
+            );
+
+            setResources(response.data);
+        })(resource);
+        },
+    [resource]
+    );
+
+    return (
+        <div>
+            <ul>
+            {resources.map(record => <li key={record.id} >{record.title}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+// class ResourceList extends React.Component {
+//     state = { resources: [] };
+
+//     async componentDidMount() {
+//         const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+//         this.setState({ resources: response.data });
+//     }
+
+//     render() {
+//         return <div>{this.state.resources.length}</div>;
+//     }
+// }
+
+
+
+export default ResourceList;
